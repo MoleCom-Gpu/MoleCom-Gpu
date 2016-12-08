56a5ed95a5a0e4f883802f28a41293dbde1f133a
Kerim Gokarslan <kerim.gokarslan@boun.edu.tr>
2016-12-08 15:48:20 +0300

    fixed single receiver tranmission ctrl

--- cudatensor.lua
+++ cudatensor.lua
@@ -1,13 +1,18 @@
 cutorch = require 'cutorch'
 
-simulationRunTime = 1000
-numberOfMolecules = 10000000
+diffusionCoefficient = 2.10e-5
+deltaTime = 0.001
+twoDT = (2 * diffusionCoefficient * deltaTime)
+simulationRunTime = 0.002
+numberOfMolecules = 100
+loopLength = simulationRunTime/deltaTime 
 molecules = torch.CudaTensor(3, numberOfMolecules): uniform(0, 10)
 availability = torch.CudaTensor(numberOfMolecules): fill(1)
-for i = 1, simulationRunTime do
-	delta1 = torch.CudaTensor(numberOfMolecules): normal(0, 10)
-	delta2 = torch.CudaTensor(numberOfMolecules): normal(0, 10)  
-	delta3 = torch.CudaTensor(numberOfMolecules): normal(0, 10) 
+
+for i = 1, loopLength do
+	delta1 = torch.CudaTensor(numberOfMolecules): normal(0, twoDT)
+	delta2 = torch.CudaTensor(numberOfMolecules): normal(0, twoDT)  
+	delta3 = torch.CudaTensor(numberOfMolecules): normal(0, twoDT) 
 	molecules[1]: add(delta1:cmul(availability))
 	molecules[2]: add(delta2:cmul(availability))
 	molecules[3]: add(delta3:cmul(availability))
@@ -23,6 +28,6 @@ for i = 1, simulationRunTime do
 	dist = sq1:add(sq2)
 	dist:add(sq3)
 	dist:pow(0.5)
-	availability = dist:le(3)
- 			
+	availability = dist:gt(3)
 end
+print(availability)
