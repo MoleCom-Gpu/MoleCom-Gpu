010a98efbb4c4ddccbd2e82d45a5dbcc91525318
Kerim Gokarslan <kerim.gokarslan@boun.edu.tr>
2016-12-20 17:50:54 +0300

    Read previously generated config file from disk

---
+++ configGenerate.lua
@@ -0,0 +1,39 @@
+cutorch = require 'cutorch'
+--JSON = require 'JSON'
+
+transmittersRadius = {}
+receiversCoordinates = {}
+tranmittersCoordinates = {}
+receiversRadius = {}
+tranmittersRadius = {}
+
+
+print 'Generating configuration file'
+mode = 0
+file = torch.DiskFile('config2', 'w')
+	--configurationString = file:readString("*a")
+
+local configurationFile = {
+	diffusionCoefficient = 79.4 ,
+	deltaTime = 0.005 ,
+	runTime = 100 ,
+	symbolSize = 100 ,
+	symbolDuration = 1 ,
+	numberOfReceivers = 1 ,
+	numberOfTranmitters = 1 ,
+
+	receiversCoordinates = { torch.CudaTensor({60, 60, 60}) },
+	transmittersCoordinates = { torch.CudaTensor({40, 40, 40}) },
+	receiversRadius = {10},
+	transmittersRadius ={10} ,
+	moleculeRadius = 2.5e-3
+}
+
+file:writeObject(configurationFile)
+file:close()
+
+file = torch.DiskFile('config2', 'r')
+configurationObject = file:readObject()
+print (configurationObject)
+
+print 'Generation completed'
