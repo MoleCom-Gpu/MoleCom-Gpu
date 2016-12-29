d5cb4cb5282f9362d3ac496b7bc50c0e06fbe918
Kerim Gokarslan <kerimgokarslan@gmail.com>
2016-12-29 18:01:48 +0300

    Transmission from transmitter surface is added

--- bm-simulator.lua
+++ bm-simulator.lua
@@ -27,8 +27,6 @@ function readConfiguration()
 	file = torch.DiskFile('config2', 'r')
 	configurationObject = file:readObject()
 
-
-
 	diffusionCoefficient = configurationObject.diffusionCoefficient
 	deltaTime = configurationObject.deltaTime
 	runTime = configurationObject.runTime
@@ -62,14 +60,45 @@ function readConfiguration()
 	print 'Configuration file is read.'
 	
 end
-
+-- It generates molecules for the transmitter. Algorithm works as follows
+-- First generate coordinates of a molecule supposing the sphere centered at the origin, then move the points by center of the sphere
+-- To generate coordinates first generate x and y coordinates then find z by the fact that a point on a surface of a sphere is exactly far from radius of that sphere.
+function generateMolecules(transmitterNumber)
+	-- radius of the given transmitter
+	radius = transmittersRadius[transmitterNumber]
+	-- randomly generate coordinates for x and y.
+	moleculeX = torch.CudaTensor(1, symbolSize): uniform(-1 * radius, radius)
+	moleculeY = torch.CudaTensor(1, symbolSize): uniform(-1 * radius, radius)
+	-- if x^2 + y^2 >= r^2, set y^2 = r^2 - x^2 (This might be changed in order to improve randomness)
+        sqMoleculeX = torch.cmul(moleculeX, moleculeX)
+        sqMoleculeY = torch.cmul(moleculeY, moleculeY)
+        flag = (sqMoleculeX + sqMoleculeY - radius * radius):ge(0) 
+	subs = torch.pow(radius * radius - sqMoleculeX, 0.5)
+        moleculeY = torch.cmul(flag, subs) + torch.cmul(1 - flag, moleculeY)
+	-- now set z as sqrt(r^2-x^2-y^2)
+         moleculeZ = torch.abs(radius * radius - sqMoleculeX - torch.cmul(moleculeY, moleculeY)) -- in order to avoid negative values caused by double precision.
+	moleculeZ:pow(0.5)
+        --print(moleculeX, moleculeY, moleculeZ)
+	-- now randomly set sign of z coordinate (by square root only positive values are retrieved but the occurance of positive and negative values are equally likely. 
+        signGenerator = torch.CudaTensor(1, symbolSize): uniform(-1, 1)
+        signGenerator = signGenerator:ge(0)
+	moleculeZ = torch.cmul(signGenerator, moleculeZ) + torch.cmul(1 - signGenerator, -moleculeZ)
+	-- check: all values of this tensor should be radius^2 
+	--print(torch.cmul(moleculeX, moleculeX) + torch.cmul(moleculeY, moleculeY) + torch.cmul(moleculeZ, moleculeZ))
+	-- move all points by center of the sphere
+	moleculeX:add(transmittersCoordinates[transmitterNumber][1])
+	moleculeY:add(transmittersCoordinates[transmitterNumber][2])
+	moleculeZ:add(transmittersCoordinates[transmitterNumber][3])
+	return torch.cat(torch.cat(moleculeX, moleculeY, 1), moleculeZ, 1)
+end
 readConfiguration()
 twoDT = (2 * diffusionCoefficient * deltaTime)
 loopLength = runTime/deltaTime
 symbolCheck = symbolDuration/deltaTime 
 numberOfSymbols = runTime/symbolDuration
 --numberOfMolecules = numberOfSymbols * symbolSize
-molecules = torch.CudaTensor(3, symbolSize): uniform(30, 50)
+molecules = generateMolecules(1)
+
 availability = torch.CudaTensor(symbolSize): fill(1)
 receiverCount = torch.CudaTensor(numberOfSymbols): fill(0) -- it might be also a double tensor
 for i = 1, loopLength do
@@ -78,7 +107,7 @@ for i = 1, loopLength do
 		-- evaluation of previous symbol
 		receiverCount[symbolNumber] = availability:size(1) - torch.sum(availability) - torch.sum(receiverCount)
 		-- if symbol is 1, single transmitter
-		molecules = torch.cat(molecules, torch.CudaTensor(3, symbolSize): uniform(0, 100))
+		molecules = torch.cat(molecules, generateMolecules(1))
 		availability = torch.cat(availability, torch.CudaTensor(symbolSize):fill(1))
 		
 	end
